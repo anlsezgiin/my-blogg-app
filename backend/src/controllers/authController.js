756a984e3f44abee71c0482a85b6963c8f425185
Anıl Sezgin <anlsezgiin@192.168.1.108>
2025-02-20 07:09:21 +0300

    login process added

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,5 @@
 import { createUser } from "../models/userModel.js";
+import { loginProcess } from "../models/userModel.js";
 
 export const registerUser = async (req, res) => {
     try {
@@ -16,3 +17,27 @@ export const registerUser = async (req, res) => {
         res.status(500).json({ error: "Sunucu hatası" });
     }
 };
+
+export const loginUser = async (req,res) => {
+    try
+    {
+        let {username, password} = req.body;
+        if (!username || !password) {
+            return res.status(400).json({ error: "Username ve Password zorunludur!" });
+        }
+        const result = await loginProcess(username,password);
+        if(result.rows.length>0)
+        {
+            res.status(200).json({message: "Giriş Başarılı!"});
+        }
+        else
+        {
+            res.status(401).json({message: "Kullanıcı adı veya şifre hatalı!"});
+        }
+    }
+    catch(err)
+    {
+        console.error("Something bad happened:",err);
+        res.status(500).json({error: "Sunucu hatası"});
+    }
+}
--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -5,3 +5,7 @@ const db = connectDB();
 export const createUser = async (username, password) => {
     return await db.query("INSERT INTO users (username, password) VALUES ($1, $2)", [username, password]);
 };
+
+export const loginProcess = async (username, password) => {
+    return await db.query("SELECT * FROM users where username=$1 and password=$2", [username,password]);
+};
